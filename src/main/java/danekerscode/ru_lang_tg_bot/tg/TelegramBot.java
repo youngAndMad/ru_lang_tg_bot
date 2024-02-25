5a174707e79c72594c212e876e0e35cf88f77488
youngandmad <kkraken2005@gmail.com>
2024-02-25 18:40:15 +0600

    greeting action

--- src/main/java/danekerscode/ru_lang_tg_bot/tg/TelegramBot.java
+++ src/main/java/danekerscode/ru_lang_tg_bot/tg/TelegramBot.java
@@ -4,7 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+import static danekerscode.ru_lang_tg_bot.tg.TgBotConstants.GREETING;
 
 @Component
 @Slf4j
@@ -28,6 +38,39 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText());
+        var chatId = update.getMessage().getChatId();
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            var text = update.getMessage().getText();
+            log.info("Received message: chat.id = {}, text = {}", chatId, text);
+            if (text.equals("/start")) {
+                greeting(update.getMessage());
+            }
+        }
     }
+
+    private void greeting(Message message) {
+        String chatId = String.valueOf(message.getChatId());
+        var sendMessage = new SendMessage(
+                chatId,
+                GREETING.formatted(message.getFrom().getUserName())
+        );
+
+        var replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+        replyKeyboardMarkup.setKeyboard(List.of(
+                new KeyboardRow(List.of(new KeyboardButton("Погнали!"))),
+                new KeyboardRow(List.of(new KeyboardButton("Не хочу")))
+        ));
+
+        replyKeyboardMarkup.setResizeKeyboard(true);
+
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        try {
+            executeAsync(sendMessage).thenRun(() -> log.info("Greeting message sent: chat.id = {}", chatId));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
---
+++ src/main/java/danekerscode/ru_lang_tg_bot/tg/TgBotConstants.java
@@ -0,0 +1,14 @@
+package danekerscode.ru_lang_tg_bot.tg;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TgBotConstants {
+
+    // add stickers please
+    public static final String GREETING = """ 
+            📝 Привет %s!
+            Я бот для улучшения ораторского и публичного выступления.
+            Если хочешь начать обучения нажми 'Погнали'!
+            """;
+}
