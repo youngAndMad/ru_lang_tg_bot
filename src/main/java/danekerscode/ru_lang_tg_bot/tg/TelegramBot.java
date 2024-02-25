340936b0ed415982918f5b6c932bc8bd02330c39
youngandmad <kkraken2005@gmail.com>
2024-02-25 19:01:49 +0600

    sync changes

--- src/main/java/danekerscode/ru_lang_tg_bot/tg/TelegramBot.java
+++ src/main/java/danekerscode/ru_lang_tg_bot/tg/TelegramBot.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -12,9 +13,10 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Arrays;
 import java.util.List;
 
-import static danekerscode.ru_lang_tg_bot.tg.TgBotConstants.GREETING;
+import static danekerscode.ru_lang_tg_bot.tg.TgBotConstants.*;
 
 @Component
 @Slf4j
@@ -42,35 +44,77 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             var text = update.getMessage().getText();
             log.info("Received message: chat.id = {}, text = {}", chatId, text);
-            if (text.equals("/start")) {
-                greeting(update.getMessage());
+            switch (text) {
+                case START -> greeting(update.getMessage());
+                case LETS_STUDY -> askForOratorSkillLevel(update.getMessage());
+                case NO_THANKS -> noThanks(update.getMessage());
+                case ADVANCED_ORATOR_SKILL_LEVEL,
+                        BEGIN_ORATOR_SKILL_LEVEL,
+                        MIDDLE_ORATOR_SKILL_LEVEL -> processOratorSkillLevelAnswer(update.getMessage());
             }
         }
     }
 
     private void greeting(Message message) {
         String chatId = String.valueOf(message.getChatId());
-        var sendMessage = new SendMessage(
-                chatId,
-                GREETING.formatted(message.getFrom().getUserName())
-        );
+        var sendMessage = new SendMessage(chatId, GREETING.formatted(message.getFrom().getUserName()));
 
-        var replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        var replyMarkup = createReplyKeyboardMarkup(LETS_STUDY, NO_THANKS);
+        sendMessage.setReplyMarkup(replyMarkup);
 
-        replyKeyboardMarkup.setKeyboard(List.of(
-                new KeyboardRow(List.of(new KeyboardButton("Погнали!"))),
-                new KeyboardRow(List.of(new KeyboardButton("Не хочу")))
-        ));
+        processMessageSending(sendMessage);
+        log.info("Greeting message sent: chat.id = {}", chatId);
+    }
 
-        replyKeyboardMarkup.setResizeKeyboard(true);
+    private void askForOratorSkillLevel(Message message) {
+        String chatId = String.valueOf(message.getChatId());
+        var sendMessage = new SendMessage(chatId, ORATOR_SKILL_LEVEL);
+        var replyMarkup = createReplyKeyboardMarkup(BEGIN_ORATOR_SKILL_LEVEL, MIDDLE_ORATOR_SKILL_LEVEL, ADVANCED_ORATOR_SKILL_LEVEL);
+        sendMessage.setReplyMarkup(replyMarkup);
 
-        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        processMessageSending(sendMessage);
+        log.info("Asking for orator skill level: chat.id = {}", chatId);
+    }
 
+    private void noThanks(Message message) {
+        String chatId = String.valueOf(message.getChatId());
+        var sendMessage = new SendMessage(chatId, NO_THANKS_TEXT);
+        processMessageSending(sendMessage);
+        log.info("No thanks message sent: chat.id = {}", chatId);
+    }
+
+    private void processOratorSkillLevelAnswer(Message message) {
+        var chatId = String.valueOf(message.getChatId());
+        var skillLevel = message.getText();
+
+        var sendMessage = new SendMessage(chatId, "Ты выбрал уровень: " + skillLevel + " . Теперь давай это проверим! Сейчас ты пройдешь тест для этого");
+        processMessageSending(sendMessage);
+    }
+
+    private void processMessageSending(BotApiMethod<Message> message) {
         try {
-            executeAsync(sendMessage).thenRun(() -> log.info("Greeting message sent: chat.id = {}", chatId));
+            execute(message);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private ReplyKeyboardMarkup createReplyKeyboardMarkup(String... buttons) {
+        var replyMarkup = new ReplyKeyboardMarkup();
+
+        replyMarkup.setResizeKeyboard(true);
+        replyMarkup.setOneTimeKeyboard(true);
+        replyMarkup.setSelective(true);
+
+
+        replyMarkup.setKeyboard(List.of(new KeyboardRow
+                (List.of(
+                        Arrays.stream(buttons).map(
+                                KeyboardButton::new
+                        ).toArray(KeyboardButton[]::new)
+                )))
+        );
+
+        return replyMarkup;
+    }
 }
--- src/main/java/danekerscode/ru_lang_tg_bot/tg/TgBotConstants.java
+++ src/main/java/danekerscode/ru_lang_tg_bot/tg/TgBotConstants.java
@@ -5,10 +5,20 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class TgBotConstants {
 
-    // add stickers please
     public static final String GREETING = """ 
             📝 Привет %s!
             Я бот для улучшения ораторского и публичного выступления.
             Если хочешь начать обучения нажми 'Погнали'!
             """;
+    public static final String NO_THANKS_TEXT = "Жаль, что ты не хочешь улучшить свои навыки. Но если передумаешь, я всегда тут!";
+
+    public static final String START = "/start";
+
+    public static final String LETS_STUDY = "Погнали!";
+    public static final String NO_THANKS = "Не хочу";
+
+    public static final String ORATOR_SKILL_LEVEL = "На каком уровне ты сейчас?";
+    public static final String BEGIN_ORATOR_SKILL_LEVEL = "Новичок";
+    public static final String MIDDLE_ORATOR_SKILL_LEVEL = "Средний";
+    public static final String ADVANCED_ORATOR_SKILL_LEVEL = "Продвинутый";
 }
