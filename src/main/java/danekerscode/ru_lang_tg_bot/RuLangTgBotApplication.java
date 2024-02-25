ccd6fddb62a11d3ba66c40de13906b8f888ad8b4
youngandmad <kkraken2005@gmail.com>
2024-02-25 17:40:05 +0600

    first commit

---
+++ src/main/java/danekerscode/ru_lang_tg_bot/RuLangTgBotApplication.java
@@ -0,0 +1,13 @@
+package danekerscode.ru_lang_tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuLangTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuLangTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/danekerscode/ru_lang_tg_bot/RuLangTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package danekerscode.ru_lang_tg_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuLangTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
